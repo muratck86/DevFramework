b05d300cb9ccd77923e654bd1f46b720c7962138
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-07 01:11:48 +0300

    add a couple of tests into data access tests layer for ProductDal with bad code, to be refactored later.

---
+++ DevFramework.Northwind.DataAccess.Tests/EntityFrameworkUnitTests/EfProductDalUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevFramework.Northwind.DataAccess.Tests.EntityFrameworkUnitTests
+{
+    [TestClass]
+    public class EfProductDalUnitTest
+    {
+        [TestMethod]
+        public void GetList_returns_all_products_without_filter()
+        {
+            EfProductDal efProductDal = new EfProductDal();
+            var result = efProductDal.GetList();
+
+            Assert.AreEqual(78, result.Count);
+        }
+
+        [TestMethod]
+        public void GetList_returns_filtered_products_with_filter()
+        {
+            EfProductDal efProductDal = new EfProductDal();
+            var result = efProductDal.GetList(p => p.CategoryId==2);
+
+            Assert.AreEqual(14, result.Count);
+        }
+    }
+}
