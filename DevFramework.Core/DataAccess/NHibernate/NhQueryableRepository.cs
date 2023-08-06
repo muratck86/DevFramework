3e612c3b7031389443d867b847147ecea4d1a221
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-06 18:54:33 +0300

    implemented EntityRepository and Queryable Repositories for NHibernate

---
+++ DevFramework.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,26 @@
+﻿using DevFramework.Core.Entities;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private IQueryable<T> _entities;
+        private NHibernateHelper _helper;
+
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _helper = nHibernateHelper;
+        }
+
+        public virtual IQueryable<T> Entities => _entities ?? _helper.OpenSession().Query<T>();
+
+
+        public IQueryable<T> Table => this.Entities;
+    }
+}
