c3909f7a1f21559ea10db3dc828007f2e2dbc1ac
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-06 20:02:16 +0300

    some entities, context and  ProductDal in the data access layer implemented

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System.Data.Entity;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DevFramework.Northwind.Entities/Concrete/Customer.cs
@@ -0,0 +1,13 @@
+﻿using DevFramework.Core.Entities;
+
+namespace DevFramework.Northwind.Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public int CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Phone { get; set; }
+    }
+}
