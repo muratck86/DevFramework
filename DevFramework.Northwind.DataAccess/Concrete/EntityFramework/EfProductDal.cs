ea3e8199f4baea58b5fb92490bae6610c32756e5
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-08-07 03:43:55 +0300

    A demonstration of how to handle complex type entities is implemented as ProductDetails.

--- DevFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DevFramework.Core.DataAccess.EntityFramework;
 using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,23 @@ using System.Threading.Tasks;
 
 namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal: EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
+        public List<ProductDetail> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
--- DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DevFramework.Core.DataAccess.NHibernate;
 using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,26 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
     {
+        NHibernateHelper _helper;
         public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
+            _helper = nHibernateHelper;
         }
-    }
 
-    public class NhCategoryDal : NhEntityRepositoryBase<Category>, ICategoryDal
-    {
-        public NhCategoryDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        public List<ProductDetail> GetProductDetails()
         {
+            using (var session = _helper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>() on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName
+                             };
+                return result.ToList();
+            }
         }
     }
 }
